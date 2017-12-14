57f5cf6f84516a057320b51914a79cd69e486086
1185303 <divya.singh7@tcs.com>
2017-12-14 20:49:02 +0530

    login page integrated

--- script.js
+++ script.js
@@ -8,23 +8,23 @@ app.config(function($routeProvider) {
       $routeProvider
  
           .when('/', {
-             templateUrl : 'pages/login/signin.html'
-             
+             templateUrl : 'pages/login/signin.html',
+             controller:"regCtrl"
          }) 
          
-         .when('/login', {
-             templateUrl : 'pages/login/login.html'
-             // controller  : 'userCtrl'
-              }) 
+         // .when('/login', {
+         //     templateUrl : 'pages/login/login.html'
+             
+         //      }) 
         
           .when('/signup', {
-             templateUrl : 'pages/register/signup.html'
-             // controller  : 'userCtrl'
+             templateUrl : 'pages/register/signup.html',
+             controller:"signup"
               }) 
 
          .when('/register', {
-             templateUrl : 'pages/reg.html'
-             // controller  : 'userCtrl'
+             templateUrl : 'pages/register.html'
+             
             
          })
          .otherwise({
@@ -33,46 +33,60 @@ app.config(function($routeProvider) {
  });
 
 
-// <!-- hardcoding to check with routing -->
+app.controller("regCtrl", function($scope,$http,$location){
 
-app.controller('userCtrl',function($scope,$window){
-      
-          $scope.login=function()
-  { 
-if($scope.UserName=="admin" && $scope.password=="admin" )
-    {
-      $window.open("/login");
+$scope.loginUser = function(){
+       $http({
+    method: 'GET',
+    url: 'http://localhost:4000/Get'
+  }).then(
+          function successCallback(response) {
+            
+                console.log("Entered in successCallback ");
+                console.log(JSON.stringify(response.data));
+                console.log(response.status);
+                console.log(response.statusText);
+                console.log(response.statusText);
+
+        $scope.birdsapi = response.data.data;
+              for (var i=0;i<($scope.birdsapi.Data).length;i++)
+              {
+              if (($scope.userName == $scope.birdsapi.Data[i].email) && ($scope.pwd == $scope.birdsapi.Data[i].pwd) )
+              {
+
+              $location.path("/register")
+            
+                
+          }
+                } function errorCallback(response) {
+                console.log("Entered in errorCallback ");
+                console.log(response.xhrStatus);
+                console.log(response.status);
+                console.log(response.statusText);
+}
+});
+
+}
+    $scope.signUp = function(){
+        $location.path("/register")
     }
-    
-    else
-    {
-      alert("Please enter valid Username and Password");
-    }
-  }
+});
 
-
-$scope.register = function()
+app.controller('signup', function($scope,$http,$location){
+$scope.login = function(firstname,lastname,pwd,confirmpwd,email,phone)
 {
    var body = {
-            UserName: $scope.UserName,
+        
             firstname: $scope.firstname,
             lastname : $scope.lastname,
+            pwd  : $scope.pwd,
+            confirmpwd : $scope.confirmpwd,
             email:$scope.email,
-            phone:$scope.phone,
-            pwd : $scope.pwd,
-            confirmpwd : $scope.confirmpwd
-            
-        
-
+            phone:$scope.phone
   };
-   // var book = JSON.stringify(data);
-   // $http.post("/Postbird", data).success(function(data, status) {
-   //      console.log('Data posted successfully');
-   //    })
-
    $http({
     method: 'POST',
-    url: 'http://localhost:3000/Post',
+    url: 'http://localhost:4000/Post',
     data:body,
     headers:{'Content-Type': 'application/json'},
   }).then(function successCallback(response) {
@@ -82,7 +96,8 @@ $scope.register = function()
         console.log(response.statusText);
         console.log(response.statusText);
         alert('Success');
-        }, function errorCallback(response) {
+        }, 
+        function errorCallback(response) {
         console.log("Entered in errorCallback ");
         console.log(response.xhrStatus);
         console.log(response.status);
@@ -91,6 +106,3 @@ $scope.register = function()
    }
 
 });
-
- 
-
